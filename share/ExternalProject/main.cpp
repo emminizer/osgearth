86d01c14bbca84be4ff4ace61c3aeff4ad808404
gwaldron <gwaldron@gmail.com>
2026-07-28 09:45:10 -0400

    External project: fix missing GL3RealizeOperation

--- share/ExternalProject/main.cpp
+++ share/ExternalProject/main.cpp
@@ -1,23 +1,25 @@
 #include <osgEarth/MapNode>
 #include <osgEarth/TMS>
 #include <osgEarth/EarthManipulator>
+#include <osgEarth/GLUtils>
 #include <osgViewer/Viewer>
 
 int main(int argc, char** argv)
 {
     osg::ArgumentParser args(&argc, argv);
-    osgViewer::Viewer viewer(args);
-    
+
     osgEarth::initialize();
     osgEarth::setNotifyLevel(osg::INFO);
+
+    osgViewer::Viewer viewer(args);
+    viewer.setRealizeOperation(new osgEarth::GL3RealizeOperation());
+    viewer.setCameraManipulator(new osgEarth::Util::EarthManipulator(args));    
     
     auto layer = new osgEarth::TMSImageLayer();
     layer->setURL("https://readymap.org/readymap/tiles/1.0.0/7/");
     
     auto mapNode = new osgEarth::MapNode();
-    mapNode->getMap()->addLayer(layer);
-    
-    viewer.setCameraManipulator(new osgEarth::Util::EarthManipulator());
+    mapNode->getMap()->addLayer(layer);    
     
     viewer.setSceneData(mapNode);
     return viewer.run();
